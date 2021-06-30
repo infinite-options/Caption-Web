3e504311feb72c8c1f377e5741a47ebb506985f3
mayukh2002 <62223416+mayukh2002@users.noreply.github.com>
2021-06-30 11:14:09 -0700

    Stitched existing pages together with buttons.
    All the pages that have been made so far have been assigned a button that allows the user to navigate between Routes.

--- src/Components/Report.jsx
+++ src/Components/Report.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Row, Col, Card } from "reactstrap";
-import "./Report.css";
+import "../Styles/Report.css";
 
 export default function Report(props) {
   const alias = props.alias;
--- src/Components/simpleForm.jsx
+++ src/Components/simpleForm.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import "./SimpleForm.css";
+import "../Styles/SimpleForm.css";
 
 class Form extends Component {
   constructor(props) {

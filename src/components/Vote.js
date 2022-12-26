192f6f42c6370578cfc4a6d97dd4523babff1ce6
sairabbas <sair_abbas@yahoo.com>
2022-12-26 15:35:21 -0800

    navigate to ScoreBoard instead of publishing to ably to do so, whenever this is only one submitted caption

--- src/components/Vote.js
+++ src/components/Vote.js
@@ -58,7 +58,7 @@ export default function Vote(){
                 else if(tempCaptions.length === 0){
                     await postVote(null, userData)
                 }
-                channel.publish({data: {message: "Start ScoreBoard"}})
+                navigate("/ScoreBoard", { state: userData })
             }
         }
         getCaptions()
